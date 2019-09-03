6fc71e35ab4fe1c6ad03de9eaf83c640529ce519
Marit Zelinsky <maritzelinsky@me.com>
2019-09-03 05:08:07 -0500

    genres can be seen on details page, but still need to add spaces between the words

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -18,7 +18,11 @@ router.get('/', (req, res) => {
 // pulls details for the selected movie when clicked
 router.get('/details/:id', (req, res) => {
     let detailsId = req.params.id
-    let queryText = 'SELECT * FROM "movies" WHERE "id" = $1;';
+    let queryText = `SELECT "movies".title, "movies".poster, array_agg("genres".name) AS "movie_genre", "movies".description FROM "movies"
+                    JOIN "movies_genres" ON "movies_genres".movie_id = "movies".id
+                    JOIN "genres" ON "genres".id = "movies_genres".genre_id
+                    WHERE "movies".id = $1
+                    GROUP BY "movies".id;`
     pool.query(queryText, [detailsId])
         .then(result => {
             res.send(result.rows[0]);
--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,12 +2,23 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 class Details extends Component {
+    handleClickHome = () => {
+        this.props.history.push('/');
+    }
+
+    handleClickEdit = () => {
+        this.props.history.push('/edit');
+    }
+
     render() {
         return (
             <div>
                 <h2>{this.props.reduxStore.details.title}</h2>
                 <img src={this.props.reduxStore.details.poster} alt="movie poster" />
+                <h5>{this.props.reduxStore.details.movie_genre} </h5>
                 <p>{this.props.reduxStore.details.description}</p>
+                <button onClick={this.handleClickHome}>Back to List</button>
+                <button onClick={this.handleClickEdit}>Edit</button>
             </div>
             )
         
